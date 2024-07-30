2bdfc5af50d6e9b75a494452431969fd28bb939c
TheAngryPC <devnat11@gmail.com>
2024-07-30 17:10:05 -0400

    Made it pretty!

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import 'bootstrap/dist/css/bootstrap.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import styles from "./App.module.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <div className={styles.app}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </div>
+);
